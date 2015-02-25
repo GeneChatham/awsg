71a962ee09a2fc4349204ddcb7d2be301bf9ab87
GeneChatham <genechatham@gmail.com>
2015-02-25 11:41:24 -0800

    temp upload for Jill sync

--- js_default.js
+++ js_default.js
@@ -1,1 +1,15 @@
 // empty for now GHC-2/10/2015
+
+$(document).ready(function(){
+
+  // $(".map").css();
+
+  $(".map-button").click(function(e){
+    e.preventDefault();
+    console.log("Button pressed!");
+    // $(".map").css("display", "inline");
+    $(".map").slideToggle();
+    // google.maps.event.trigger(map, "resize");
+  })
+
+});
