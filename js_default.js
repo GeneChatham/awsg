0da2d0de604e394be63bea797fb1907baeedd28f
Jill Chatham <jillmchatham@gmail.com>
2015-02-26 10:37:33 -0800

    temp save

--- js_default.js
+++ js_default.js
@@ -3,7 +3,9 @@
 
 $(document).ready(function(){
 
-  // $(".map").hide();
+  // $(".map").html('<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2677.1299229122415!2d-121.969088!3d47.856446999999996!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549aa6f654c5b1e3%3A0x7a2d803ad886e1e1!2sMain+Street+Glass!5e0!3m2!1sen!2sus!4v1424902115031" width="600" height="450" frameborder="0" style="border:0"></iframe>', function() {
+  //   $(".map").hide();   
+  // });
 
   // $(".map-button").on('click', function (e) {
   //   if( $(".map").hasClass("showing") ){
