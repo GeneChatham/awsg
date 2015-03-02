593d9a80acda14909d73e45c464fdee0a2027487
genechatham <genechatham@gmail.com>
2015-03-02 12:46:52 -0800

    Map styling

--- js_default.js
+++ js_default.js
@@ -42,4 +42,16 @@ $(document).ready(function(){
     // }
   })
 
+  var styleMap = function() {
+    $('.map').css("position", "relative");
+    $('.map').css("padding-bottom", "75%");
+    $('.map').css("height", "0");
+    $('.map').css("overflow", "hidden");
+    $('.map iframe').css("position", "absolute");
+    $('.map iframe').css("top", "0");
+    $('.map iframe').css("left", "0");
+    $('.map iframe').css("width", "100% !important");
+    $('.map iframe').css("height", "100% !important");
+  }
+
 });
